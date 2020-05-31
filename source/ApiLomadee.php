f9a637f1abf09883f394564cc56ab0cf4ecd3499
Augusto Dias <59390486+cometwp@users.noreply.github.com>
2020-05-31 17:04:56 -0300

    Initial commit

---
+++ source/ApiLomadee.php
@@ -0,0 +1,7 @@
+<?php
+
+$url = "http://sandbox-api.lomadee.com/v3/". LMD_API_KEY ."/store/_all?sourceId=". LMD_SOURCE_ID;
+$ch  = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$response = json_decode(curl_exec($ch));
---
+++ source/Config.php
@@ -0,0 +1,3 @@
+<?php
+define('LMD_API_KEY', '1586524487910acc5cf70');
+define('LMD_SOURCE_ID', '36379559');
